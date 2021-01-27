1e6d9710f571d3f1a3a356c73505571ac2b559d8
thu <syaseigaaru@gmail.com>
2021-01-28 07:39:59 +0900

    conan build command

--- README.md
+++ README.md
@@ -4,13 +4,11 @@
 ```
 $cd build
 $conan install . --profile ./profiles/gcc
-$cmake . -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_CXX_COMPILER=/usr/bin/g++
-$cmake --build .
+$conan build .
 ```
 ### clang
 ```
 $cd build
 $conan install . --profile ./profiles/clang
-$cmake . -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++
-$cmake --build .
+$conan build .
 ```
--- build/conanfile.py
+++ build/conanfile.py
@@ -4,16 +4,8 @@ class GlfwProject(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     requires = "glfw/3.3.2"
     generators = "cmake"
-    # default_options = {
-    #         "poco:shared": True,
-    #         "openssl:shared": True
-    #     }
 
     def build(self):
         cmake = CMake(self)
-        cmake.definitions = {
-            "CMAKE_C_COMPILER": "/usr/bin/clang",
-            "CMAKE_CXX_COMPILER": "/usr/bin/clang++"
-        }
         cmake.configure()
         cmake.build()
