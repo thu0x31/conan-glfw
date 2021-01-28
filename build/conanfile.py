c3f00cc38e869d169082b5c0179dfa19748d9a78
thu <syaseigaaru@gmail.com>
2021-01-29 05:27:11 +0900

    rm conanfile.txt

--- build/conanfile.py
+++ build/conanfile.py
@@ -4,7 +4,12 @@ class GlfwProject(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     requires = "glfw/3.3.2"
     generators = "cmake"
+    build_dir = "ConanInstallFiles/"
 
+    def requirements(self):
+        self.requires("glfw/3.3.2")
+
+    # $conan build .
     def build(self):
         cmake = CMake(self)
         cmake.configure()
